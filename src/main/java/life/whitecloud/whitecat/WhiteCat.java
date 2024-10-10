ef0d68f252d40eb2d997179a24ae82b704d8e616
phillychi3 <phillychi3@gmail.com>
2024-10-10 15:17:15 +0800

    forge_version=47.3.11 to 47.2.18

--- src/main/java/life/whitecloud/whitecat/WhiteCat.java
+++ src/main/java/life/whitecloud/whitecat/WhiteCat.java
@@ -9,6 +9,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -20,25 +21,23 @@ public class WhiteCat {
     public static final String MODID = "whitecat";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public WhiteCat(FMLJavaModLoadingContext context) {
-        IEventBus modEventBus = context.getModEventBus();
+    public WhiteCat() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(TPSMonitor.class);
         MinecraftForge.EVENT_BUS.register(ScheduledShutdown.class);
         MinecraftForge.EVENT_BUS.register(VoteShutdown.class);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
 
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the
-        // config file for us
-        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
         LOGGER.info("Successfully loaded White Cat!");
     }
+
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
         LOGGER.info("White Cat is starting up!");
